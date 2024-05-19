f7ce3a83d19f8dbe41c3cb9d72a2a0ec2561966c
guven.uysal <gvnuysal@gmail.com>
2024-05-19 23:09:02 +0300

    Added list and views the screen

---
+++ Resources/Styles/CollectionViewDictionary.xaml.cs
@@ -0,0 +1,9 @@
+namespace maui_collection_view.Resources.Dictionaries;
+
+public partial class CollectionViewDictionary : ResourceDictionary
+{
+	public CollectionViewDictionary()
+	{
+		InitializeComponent();
+	}
+}
